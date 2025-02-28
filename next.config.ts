f54a225c8bbaca13964aad13a35593f68d877b22
WooLegend <dnwogus4260@naver.com>
2025-02-28 17:27:34 +0900

    chore: add configuration next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,29 @@
+import type { NextConfig } from "next";
+import type { WebpackConfigContext } from "next/dist/server/config-shared";
+import type { Configuration } from "webpack";
 import removeImports from "next-remove-imports";
 
-/** @type {function(import("next").NextConfig): import("next").NextConfig}} */
-const removeImportsFun = removeImports({
+const removeImportsFun: (config: NextConfig) => NextConfig = removeImports({
   // test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
   // matchImports: "\\.(less|css|scss|sass|styl)$"
 });
 
-export default removeImportsFun({
-  webpack(config) {
+const nextConfig: NextConfig = {
+  env: {
+    MONGODB_URL: process.env.MONGODB_URL,
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  webpack: (
+    config: Configuration,
+    { dev, isServer }: WebpackConfigContext
+  ): Configuration => {
+    if (!dev && !isServer) {
+      config.cache = true;
+    }
     return config;
   },
-});
+};
+
+export default removeImportsFun(nextConfig);
